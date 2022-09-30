0040cc33f6c767db3c40a41c11318bb07a885148
OlehNoskov <olega6000@gmail.com>
2022-09-30 17:24:07 +0300

    create application

---
+++ solution_faceIt/src/test/java/ua/com/solution/SolutionFaceItApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.com.solution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolutionFaceItApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
